67c02550e35c84fa0ec0e45e6f2fb2d50e11bc57
danielalfb <danielalfbarbosa@gmail.com>
2022-08-16 21:58:25 -0300

    create first editions pag

---
+++ primeira_edicao.php
@@ -0,0 +1,53 @@
+<?php
+  require_once __DIR__ . '/function.php';
+  getHeader();
+?>
+    <div class="page">
+    <section>
+      <div class="mainBanner">
+        <div class="mainInfo">
+          <div class="title">
+            <svg xmlns="http://www.w3.org/2000/svg" 
+            xmlns:xlink="http://www.w3.org/1999/xlink" 
+            version="1.1" 
+            id="Layer_1" 
+            x="0px" y="0px" 
+            viewBox="0 0 39.9 57.6" 
+            style="enable-background:new 0 0 39.9 57.6;" 
+            xml:space="preserve"
+            >
+            <style type="text/css">
+                .st0 {
+                  fill: var(--cor-rosinha);
+                }
+              </style>
+            <path class="st0" d="M39.9,28.8L11.1,57.6L0,47l18.4-18.2L0,10.6L11.1,0L39.9,28.8z"/>
+            </svg>
+            <h1 lang="pt">Semana de Cinema Negro de Belo Horizonte 2021</h1>
+            <h1 lang="en">Catalog</h1>
+          </div>
+          <p lang="pt">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nisi diam, hendrerit sed volutpat ut, lobortis nec nunc. Suspendisse vel ante id ex facilisis cursus. In vel tristique sem, ut iaculis ex. Proin auctor dui sit amet urna ultrices, nec gravida urna posuere. Praesent facilisis velit leo. Nullam interdum, tortor ut laoreet imperdiet, orci metus consectetur justo, nec pulvinar arcu enim nec enim. Duis sollicitudin vel sem nec sagittis. Nunc ultrices neque id neque mattis posuere. Suspendisse hendrerit metus ut purus vulputate semper.Sed vitae sodales lacus. Ut sit amet turpis pharetra, efficitur massa in, placerat augue. Mauris sollicitudin, tellus in pretium laoreet, mi dui tempus arcu, ultrices porta felis magna a arcu. In id rhoncus neque, condimentum mattis lorem. Aliquam viverra venenatis venenatis. Donec pharetra purus vitae ultrices dictum. Phasellus suscipit nunc in est lacinia vestibulum. Quisque vehicula nec nunc vitae lacinia. Duis lobortis, nisl vitae vehicula vestibulum, elit arcu malesuada sapien, sed congue velit velit id lectus. Fusce ut dui nisl. Etiam sapien ligula, fermentum vel interdum vel, gravida ac dui. Fusce sagittis tellus nec quam molestie venenatis. Fusce eros nisl, rutrum sit amet condimentum ut, pretium eu diam. Integer sit amet fringilla erat, bibendum ullamcorper libero. Aenean lacus lectus, congue eu auctor eu, facilisis ac mi.
+          </p>
+          <p lang="en">
+          Click here to download the PDF version of the Belo Horizonte Black Film Week catalog.</p>
+<p lang="en">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nisi diam, hendrerit sed volutpat ut, lobortis nec nunc. Suspendisse vel ante id ex facilisis cursus. In vel tristique sem, ut iaculis ex. Proin auctor dui sit amet urna ultrices, nec gravida urna posuere. Praesent facilisis velit leo. Nullam interdum, tortor ut laoreet imperdiet, orci metus consectetur justo, nec pulvinar arcu enim nec enim. Duis sollicitudin vel sem nec sagittis. Nunc ultrices neque id neque mattis posuere. Suspendisse hendrerit metus ut purus vulputate semper.Sed vitae sodales lacus. Ut sit amet turpis pharetra, efficitur massa in, placerat augue. Mauris sollicitudin, tellus in pretium laoreet, mi dui tempus arcu, ultrices porta felis magna a arcu. In id rhoncus neque, condimentum mattis lorem. Aliquam viverra venenatis venenatis. Donec pharetra purus vitae ultrices dictum. Phasellus suscipit nunc in est lacinia vestibulum. Quisque vehicula nec nunc vitae lacinia. Duis lobortis, nisl vitae vehicula vestibulum, elit arcu malesuada sapien, sed congue velit velit id lectus. Fusce ut dui nisl. Etiam sapien ligula, fermentum vel interdum vel, gravida ac dui. Fusce sagittis tellus nec quam molestie venenatis. Fusce eros nisl, rutrum sit amet condimentum ut, pretium eu diam. Integer sit amet fringilla erat, bibendum ullamcorper libero. Aenean lacus lectus, congue eu auctor eu, facilisis ac mi.
+          </p>  
+        </div>
+        <div class="mainIllus dowloadContainer">
+          <div class="pdfCover catalog-cover"></div>
+          <p lang="pt">Confira o catálogo do Festival.</p>
+          <p lang="en">Check out the Festival catalog.</p>
+          <a lang="pt" href="./docs/Catalogo_Semana_Cinema_Ngero_BH.pdf" download="Catálogo Semana de Cinema Negro BH" class="btn download" title="Download">DOWNLOAD PDF</a>
+          <a lang="en" href="./docs/Catalogo_Semana_Cinema_Ngero_BH.pdf" download="Belo Horizonte Black Film Week_Catalog" class="btn download" title="Download">DOWNLOAD PDF</a>
+        </div>
+      </div>
+    </section>
+    </div>
+    <?php
+  getFooterOpen(); 
+?>
+  <script src="js/search.js"></script>
+  <script src="js/main.js"></script>
+  <script>translate();</script>
+</html>
