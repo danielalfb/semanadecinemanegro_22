63e55f4b8895005c96fa5295a3f6b6ef8481aabe
Lidiane Pereira <lidianemara93@gmail.com>
2022-09-07 21:28:41 -0300

    add image nicho

--- mostras/nicho_54.php
+++ mostras/nicho_54.php
@@ -2,39 +2,67 @@
 require_once __DIR__ . '../../function.php';
 getHeaderMostras();
 ?>
-    <div class="page">
+<div class="page">
     <section>
-      <div class="mainBanner" style="background-image: url('./img/banners/banner12.svg'); height: 400px">
-        <div class="mainInfo" style="color:var(--cor-yellow);margin-top:-80px">
-          <div class="title">
-            <h1 lang="pt">SEMANA DE CINEMA NEGRO</h1>
-            <h1 lang="pt" style="margin-left:100px">DE BELO HORIZONTE CONVIDA</h1>
-            <h1 lang="pt" style="color: var(--cor-lightpink);margin-left:200px;text-align:center;">NICHO 54</h1>
-            <h1 lang="en">SEMANA DE CINEMA NEGRO</h1>
-            <h1 lang="en" style="margin-left:100px">DE BELO HORIZONTE CONVIDA</h1>
-            <h1 lang="en" style="color: var(--cor-lightpink);margin-left:200px;text-align:center;">NICHO 54</h1>
-          </div>
-         </div>
-      </div>
+        <div class="mainBanner" style="background-image: url('./img/banners/banner12.svg'); height: 400px">
+            <div class="mainInfo" style="color:var(--cor-yellow);margin-top:-80px">
+                <div class="title">
+                    <h1 lang="pt">SEMANA DE CINEMA NEGRO</h1>
+                    <h1 lang="pt" style="margin-left:100px">DE BELO HORIZONTE CONVIDA</h1>
+                    <h1 lang="pt" style="color: var(--cor-lightpink);margin-left:200px;text-align:center;">NICHO 54</h1>
+                    <h1 lang="en">SEMANA DE CINEMA NEGRO</h1>
+                    <h1 lang="en" style="margin-left:100px">DE BELO HORIZONTE CONVIDA</h1>
+                    <h1 lang="en" style="color: var(--cor-lightpink);margin-left:200px;text-align:center;">NICHO 54</h1>
+                </div>
+            </div>
+        </div>
     </section>
     <section>
-      <section>
-      <div class="mainSubtitle" style="border-left: 5px solid var(--cor-lilac);">
-        <p lang="pt">O NICHO 54 nasceu como instituto em novembro de 2019 e tem como missão contribuir para o fortalecimento das carreiras de pessoas negras no audiovisual. O festival anual NICHO Novembro realizou a mostra <em>América negra: conversas entre as negritudes latino-americanas</em> que emergiu de um projeto curatorial lançando à pergunta: "o que entraria em nosso campo de visão se nós, pessoas negras e latinas, fizéssemos o exercício de, ao conversarmos sobre vivências pretas, descentralizássemos os Estados Unidos como principal interlocutor ou território que provê narrativas?”. Destacando a importância desse debate, a Semana de Cinema Negro de Belo Horizonte convidou Heitor Augusto, um dos idealizadores do NICHO 54 e curador da mostra <em>América negra</em>, para compor esta sessão. Em “versão <em>pocket</em>”, serão exibidos 2 filmes da mostra <em>América negra: Chimbumbe</em> (Antonio Coello, Colômbia, México, 2008-2009) e <em>Negra</em> (Medhin Tewolde Serrano México, 2020). Contaremos com a presença de Heitor Augusto para uma conversa ao final da sessão.</p>
-        <p lang="en">O NICHO 54 nasceu como instituto em novembro de 2019 e tem como missão contribuir para o fortalecimento das carreiras de pessoas negras no audiovisual. O festival anual NICHO Novembro realizou a mostra <em>América negra: conversas entre as negritudes latino-americanas</em> que emergiu de um projeto curatorial lançando à pergunta: "o que entraria em nosso campo de visão se nós, pessoas negras e latinas, fizéssemos o exercício de, ao conversarmos sobre vivências pretas, descentralizássemos os Estados Unidos como principal interlocutor ou território que provê narrativas?”. Destacando a importância desse debate, a Semana de Cinema Negro de Belo Horizonte convidou Heitor Augusto, um dos idealizadores do NICHO 54 e curador da mostra <em>América negra</em>, para compor esta sessão. Em “versão <em>pocket</em>”, serão exibidos 2 filmes da mostra <em>América negra: Chimbumbe</em> (Antonio Coello, Colômbia, México, 2008-2009) e <em>Negra</em> (Medhin Tewolde Serrano México, 2020). Contaremos com a presença de Heitor Augusto para uma conversa ao final da sessão. </p>
-      </div>
-    </section>
-    <section>
-    <section>
-      <div class="container containerColumn"></div>
-    </section>
-    </div>
+        <section>
+            <div class="mainSubtitle" style="border-left: 5px solid var(--cor-lilac);">
+                <p lang="pt">O NICHO 54 nasceu como instituto em novembro de 2019 e tem como missão contribuir para o
+                    fortalecimento das carreiras de pessoas negras no audiovisual. O festival anual NICHO Novembro
+                    realizou a mostra <em>América negra: conversas entre as negritudes latino-americanas</em> que
+                    emergiu de um projeto curatorial lançando à pergunta: "o que entraria em nosso campo de visão se
+                    nós, pessoas negras e latinas, fizéssemos o exercício de, ao conversarmos sobre vivências pretas,
+                    descentralizássemos os Estados Unidos como principal interlocutor ou território que provê
+                    narrativas?”. Destacando a importância desse debate, a Semana de Cinema Negro de Belo Horizonte
+                    convidou Heitor Augusto, um dos idealizadores do NICHO 54 e curador da mostra <em>América
+                        negra</em>, para compor esta sessão. Em “versão <em>pocket</em>”, serão exibidos 2 filmes da
+                    mostra <em>América negra: Chimbumbe</em> (Antonio Coello, Colômbia, México, 2008-2009) e
+                    <em>Negra</em> (Medhin Tewolde Serrano México, 2020). Contaremos com a presença de Heitor Augusto
+                    para uma conversa ao final da sessão.
+                </p>
+                <p lang="en">O NICHO 54 nasceu como instituto em novembro de 2019 e tem como missão contribuir para o
+                    fortalecimento das carreiras de pessoas negras no audiovisual. O festival anual NICHO Novembro
+                    realizou a mostra <em>América negra: conversas entre as negritudes latino-americanas</em> que
+                    emergiu de um projeto curatorial lançando à pergunta: "o que entraria em nosso campo de visão se
+                    nós, pessoas negras e latinas, fizéssemos o exercício de, ao conversarmos sobre vivências pretas,
+                    descentralizássemos os Estados Unidos como principal interlocutor ou território que provê
+                    narrativas?”. Destacando a importância desse debate, a Semana de Cinema Negro de Belo Horizonte
+                    convidou Heitor Augusto, um dos idealizadores do NICHO 54 e curador da mostra <em>América
+                        negra</em>, para compor esta sessão. Em “versão <em>pocket</em>”, serão exibidos 2 filmes da
+                    mostra <em>América negra: Chimbumbe</em> (Antonio Coello, Colômbia, México, 2008-2009) e
+                    <em>Negra</em> (Medhin Tewolde Serrano México, 2020). Contaremos com a presença de Heitor Augusto
+                    para uma conversa ao final da sessão.
+                </p>
+                <div>
+                    <img src="./img/NICHO_54_MARCA_OFICIAL-01.png" alt="">
+                </div>
+            </div>
+        </section>
+        <section>
+            <section>
+                <div class="container containerColumn"></div>
+            </section>
+</div>
 <?php
   getFooterOpen(); 
-?>   
-    <script src="../js/search.js"></script>
-    <script src="../js/nicho54.js"></script>
-    <script src="../js/collapse.js"></script>
-    <script src="../js/main.js"></script>
-  </body>
+?>
+<script src="../js/search.js"></script>
+<script src="../js/nicho54.js"></script>
+<script src="../js/collapse.js"></script>
+<script src="../js/main.js"></script>
+</body>
+
 </html>
