5c5dce8182722e2729a827ae68f247b55e065ea8
Lidiane Pereira <lidianemara93@gmail.com>
2022-09-09 10:56:20 -0300

    fix css masterclass

--- masterclass.php
+++ masterclass.php
@@ -2,27 +2,30 @@
 require_once __DIR__ . '/function.php';
 getHeader();
 ?>
-  <div class="page">
-   <section>
-      <div class="mainBanner" style="background-color: var(--cor-blue); margin-top: 50px; height: 200px">
-        <div class="mainInfo" style="color: var(--cor-lightpink)">
-          <div class="title">
-            <h1 lang="pt">MASTERCLASS</h1>
-            <h1 lang="en">MASTERCLASS</h1>
-          </div>
-         </div>
-      </div>
+<div class="page">
+    <section>
+        <div class="mainBanner" style="background-color: var(--cor-blue); margin-top: 50px; height: 200px">
+            <div class="mainInfo mainInfoMaster" style="color: var(--cor-lightpink)">
+                <div class="title">
+                    <h1 lang="pt">MASTERCLASS</h1>
+                    <h1 lang="en">MASTERCLASS</h1>
+                </div>
+            </div>
+        </div>
     </section>
     <section>
-      <div class="container containerOficina"></div>
+        <div class="container containerOficina"></div>
     </section>
-  </div>
+</div>
 <?php
 getFooterOpen();
 ?>
 <script src="js/search.js"></script>
 <script src="js/masterclass.js"></script>
 <script src="js/main.js"></script>
-<script>translate();</script>
-  </body>
+<script>
+translate();
+</script>
+</body>
+
 </html>
