bde02e7c061d44c4b7d67557a6f57b9ba2d0449e
Lidiane Pereira <lidianemara93@gmail.com>
2022-09-09 18:14:12 -0300

    video ok

--- index.php
+++ index.php
@@ -3,119 +3,119 @@ require_once __DIR__ . '/function.php';
 getHeader();
 ?>
 <div class="page">
-  <!--banner:-->
-  <section>
-    <div class="container-inicio"
-      style="background-image: url('./mostras/img/banners/banner2.jpg'); background-position: center; background-repeat: no-repeat; background-size: cover;">
-      <div class="paragrafo-inicio">
-        <h1 lang="pt">Sobre a semana</h1>
-        <h1 lang="en">About the Week</h1>
-        <p lang="pt">
-          A 2º Semana de Cinema Negro de Belo Horizonte apresenta, de 08 a 15 de setembro de 2022, um conjunto
-          composto por 53 filmes de cinematografias africanas, negras brasileiras e da diáspora distribuídos
-          em 3 mostras, 2 homenagens e 1 Sessão Especial: <em>Por Outros Cinemas Africanos; Med Hondo: cinema
-            e
-            liberadade; Cine-Escrituras Pretas; Sessão Homenagem Maria José Novais Oliveira: Rejane Faria –
-            no
-            palco e nas telas; Semana de Cinema Negro convida a Nicho 54; e Ibejis (Infatil)</em>.
-        </p>
-        <p lang="en">
-          A 2º Semana de Cinema Negro de Belo Horizonte apresenta, de 08 a 15 de setembro de 2022, um conjunto
-          composto por 53 filmes de cinematografias africanas, negras brasileiras e da diáspora distribuídos
-          em 3 mostras, 2 homenagens e 1 Sessão Especial: <em>Por Outros Cinemas Africanos; Med Hondo: cinema
-            e
-            liberadade; Cine-Escrituras Pretas; Sessão Homenagem Maria José Novais Oliveira: Rejane Faria –
-            no
-            palco e nas telas; Semana de Cinema Negro convida a Nicho 54; e Ibejis (Infatil)</em>.
+    <!--banner:-->
+    <section>
+        <div class="container-inicio"
+            style="background-image: url('./mostras/img/banners/banner2.jpg'); background-position: center; background-repeat: no-repeat; background-size: cover;">
+            <div class="paragrafo-inicio">
+                <h1 lang="pt">Sobre a semana</h1>
+                <h1 lang="en">About the Week</h1>
+                <p lang="pt">
+                    A 2º Semana de Cinema Negro de Belo Horizonte apresenta, de 08 a 15 de setembro de 2022, um conjunto
+                    composto por 53 filmes de cinematografias africanas, negras brasileiras e da diáspora distribuídos
+                    em 3 mostras, 2 homenagens e 1 Sessão Especial: <em>Por Outros Cinemas Africanos; Med Hondo: cinema
+                        e
+                        liberadade; Cine-Escrituras Pretas; Sessão Homenagem Maria José Novais Oliveira: Rejane Faria –
+                        no
+                        palco e nas telas; Semana de Cinema Negro convida a Nicho 54; e Ibejis (Infatil)</em>.
+                </p>
+                <p lang="en">
+                    A 2º Semana de Cinema Negro de Belo Horizonte apresenta, de 08 a 15 de setembro de 2022, um conjunto
+                    composto por 53 filmes de cinematografias africanas, negras brasileiras e da diáspora distribuídos
+                    em 3 mostras, 2 homenagens e 1 Sessão Especial: <em>Por Outros Cinemas Africanos; Med Hondo: cinema
+                        e
+                        liberadade; Cine-Escrituras Pretas; Sessão Homenagem Maria José Novais Oliveira: Rejane Faria –
+                        no
+                        palco e nas telas; Semana de Cinema Negro convida a Nicho 54; e Ibejis (Infatil)</em>.
 
-        </p>
-        <p lang="pt">
-          As exibições presenciais serão realizadas no Cine Humberto Mauro/ Palácio das Artes e no Cine Santa
-          Tereza, toda a programação é gratuita. O festival acontecerá de forma híbrida, presencial e on-line.
-          Os filmes da mostra <em>Cine-Escrituras Pretas</em> estarão disponíveis nas salas de exibição e
-          também na
-          plataforma Cine Humberto Mauro Mais <a href="https://www.cinehumbertomauromais.com" target="_blank"
-            rel="noopener noreferrer">(https://www.cinehumbertomauromais.com)</a>. No nosso site, além das
-          informações dos filmes, é possível acessar um link que direciona para a plataforma dos filmes
-          on-line. Nesta edição, além das sessões fílmicas, teremos conversas com convidadas e convidados
-          internacionais, bem como debates com realizadoras e realizadores na mostra dedicada aos filmes
-          brasileiros – todos os debates acontecerão de forma presencial. Contamos, ainda, com duas oficinas:
-          <em>Por Outros Cinemas Africanos</em>, ministrada por Janaína Oliveira (presencial), e <em>Processo
-            Criativo</em> e
-          <em>Mercado de Trabalho para Roteiristas</em>, ministrada por Ana do Carmo e realizada em parceria
-          com a
-          Paradiso Multiplica (on-line). Apresentaremos também a masterclass <em>Marte Um: do sonho a
-            realização</em>,
-          ministrada pelo diretor Gabriel Martins, que irá compartilhar o processo de desenvolvimento do seu
-          primeiro filme solo de longa-metragem (presencial).
+                </p>
+                <p lang="pt">
+                    As exibições presenciais serão realizadas no Cine Humberto Mauro/ Palácio das Artes e no Cine Santa
+                    Tereza, toda a programação é gratuita. O festival acontecerá de forma híbrida, presencial e on-line.
+                    Os filmes da mostra <em>Cine-Escrituras Pretas</em> estarão disponíveis nas salas de exibição e
+                    também na
+                    plataforma Cine Humberto Mauro Mais <a href="https://www.cinehumbertomauromais.com" target="_blank"
+                        rel="noopener noreferrer">(https://www.cinehumbertomauromais.com)</a>. No nosso site, além das
+                    informações dos filmes, é possível acessar um link que direciona para a plataforma dos filmes
+                    on-line. Nesta edição, além das sessões fílmicas, teremos conversas com convidadas e convidados
+                    internacionais, bem como debates com realizadoras e realizadores na mostra dedicada aos filmes
+                    brasileiros – todos os debates acontecerão de forma presencial. Contamos, ainda, com duas oficinas:
+                    <em>Por Outros Cinemas Africanos</em>, ministrada por Janaína Oliveira (presencial), e <em>Processo
+                        Criativo</em> e
+                    <em>Mercado de Trabalho para Roteiristas</em>, ministrada por Ana do Carmo e realizada em parceria
+                    com a
+                    Paradiso Multiplica (on-line). Apresentaremos também a masterclass <em>Marte Um: do sonho a
+                        realização</em>,
+                    ministrada pelo diretor Gabriel Martins, que irá compartilhar o processo de desenvolvimento do seu
+                    primeiro filme solo de longa-metragem (presencial).
 
-        </p>
-        <p lang="en">
-          As exibições presenciais serão realizadas no Cine Humberto Mauro/ Palácio das Artes e no Cine Santa
-          Tereza, toda a programação é gratuita. O festival acontecerá de forma híbrida, presencial e on-line.
-          Os filmes da mostra <em>Cine-Escrituras Pretas</em> estarão disponíveis nas salas de exibição e
-          também na
-          plataforma Cine Humberto Mauro Mais <a href="https://www.cinehumbertomauromais.com" target="_blank"
-            rel="noopener noreferrer">(https://www.cinehumbertomauromais.com)</a>. No nosso site, além das
-          informações dos filmes, é possível acessar um link que direciona para a plataforma dos filmes
-          on-line. Nesta edição, além das sessões fílmicas, teremos conversas com convidadas e convidados
-          internacionais, bem como debates com realizadoras e realizadores na mostra dedicada aos filmes
-          brasileiros – todos os debates acontecerão de forma presencial. Contamos, ainda, com duas oficinas:
-          <em>Por Outros Cinemas Africanos</em>, ministrada por Janaína Oliveira (presencial), e <em>Processo
-            Criativo</em> e
-          <em>Mercado de Trabalho para Roteiristas</em>, ministrada por Ana do Carmo e realizada em parceria
-          com a
-          Paradiso Multiplica (on-line). Apresentaremos também a masterclass <em>Marte Um: do sonho a
-            realização</em>,
-          ministrada pelo diretor Gabriel Martins, que irá compartilhar o processo de desenvolvimento do seu
-          primeiro filme solo de longa-metragem (presencial).
+                </p>
+                <p lang="en">
+                    As exibições presenciais serão realizadas no Cine Humberto Mauro/ Palácio das Artes e no Cine Santa
+                    Tereza, toda a programação é gratuita. O festival acontecerá de forma híbrida, presencial e on-line.
+                    Os filmes da mostra <em>Cine-Escrituras Pretas</em> estarão disponíveis nas salas de exibição e
+                    também na
+                    plataforma Cine Humberto Mauro Mais <a href="https://www.cinehumbertomauromais.com" target="_blank"
+                        rel="noopener noreferrer">(https://www.cinehumbertomauromais.com)</a>. No nosso site, além das
+                    informações dos filmes, é possível acessar um link que direciona para a plataforma dos filmes
+                    on-line. Nesta edição, além das sessões fílmicas, teremos conversas com convidadas e convidados
+                    internacionais, bem como debates com realizadoras e realizadores na mostra dedicada aos filmes
+                    brasileiros – todos os debates acontecerão de forma presencial. Contamos, ainda, com duas oficinas:
+                    <em>Por Outros Cinemas Africanos</em>, ministrada por Janaína Oliveira (presencial), e <em>Processo
+                        Criativo</em> e
+                    <em>Mercado de Trabalho para Roteiristas</em>, ministrada por Ana do Carmo e realizada em parceria
+                    com a
+                    Paradiso Multiplica (on-line). Apresentaremos também a masterclass <em>Marte Um: do sonho a
+                        realização</em>,
+                    ministrada pelo diretor Gabriel Martins, que irá compartilhar o processo de desenvolvimento do seu
+                    primeiro filme solo de longa-metragem (presencial).
 
 
-        </p>
-        <p lang="pt">A 2ª edição da Semana de Cinema Negro irá acontecer presencialmente pela primeira vez e por
-          esse motivo se torna essencial a participação do público, para a construção do festival.
-          Promoveremos um encontro entre os filmes e o público marcado pelo afeto e acolhimento. Continuamos a
-          criar memórias, tanto através da identidade visual, quanto na proposta da curadoria da mostra
-          <em>Por
-            Outros Cinemas Africanos</em> que pretende olhar para o passado para nos ajudar a construir o
-          presente e
-          pensar novos futuros.
-        </p>
-        <p lang="en">
-          A 2ª edição da Semana de Cinema Negro irá acontecer presencialmente pela primeira vez e por
-          esse motivo se torna essencial a participação do público, para a construção do festival.
-          Promoveremos um encontro entre os filmes e o público marcado pelo afeto e acolhimento. Continuamos a
-          criar memórias, tanto através da identidade visual, quanto na proposta da curadoria da mostra
-          <em>Por
-            Outros Cinemas Africanos</em> que pretende olhar para o passado para nos ajudar a construir o
-          presente e
-          pensar novos futuros.
-        </p>
-        <p lang="pt">
-          Concebemos um catálogo com textos que refletem sobre a mostra <em>Por Outros Cinemas Africanos</em>,
-          escrito
-          por Janaína Oliveira. Traremos duas entrevistas no catálogo: uma com Annabelle Aventurin, publicada
-          na revista <em>Non Fiction</em>, na qual fala sobre processos de restauração dos filmes do cineasta
-          Med
-          Hondo; outra realizada por Alessandra Brito com Rejane Faria, a nossa homenageada da edição.
-        </p>
-        <p lang="pt">Convidamos a todas, todes e todos a acompanhar a programação.</p>
-        <p lang="en">We invite all of you to keep up with the program.</p>
-      </div>
-    </div>
-  </section>
-  <section class="vinheta">
-    <div class="video">
-      <button id="button"><em class="fa fa-play" aria-hidden="true"></em></button>
-      <div id="lightbox">
-        <em id="close-btn" class="fa fa-times"></em>
-        <div id="video-wrapper">
-          <iframe src="https://drive.google.com/file/d/1rN08kcApfcr2um2vRhaRPxk75fHDlagc/view" width="960" height="540"
-            allowfullscreen></iframe>
+                </p>
+                <p lang="pt">A 2ª edição da Semana de Cinema Negro irá acontecer presencialmente pela primeira vez e por
+                    esse motivo se torna essencial a participação do público, para a construção do festival.
+                    Promoveremos um encontro entre os filmes e o público marcado pelo afeto e acolhimento. Continuamos a
+                    criar memórias, tanto através da identidade visual, quanto na proposta da curadoria da mostra
+                    <em>Por
+                        Outros Cinemas Africanos</em> que pretende olhar para o passado para nos ajudar a construir o
+                    presente e
+                    pensar novos futuros.
+                </p>
+                <p lang="en">
+                    A 2ª edição da Semana de Cinema Negro irá acontecer presencialmente pela primeira vez e por
+                    esse motivo se torna essencial a participação do público, para a construção do festival.
+                    Promoveremos um encontro entre os filmes e o público marcado pelo afeto e acolhimento. Continuamos a
+                    criar memórias, tanto através da identidade visual, quanto na proposta da curadoria da mostra
+                    <em>Por
+                        Outros Cinemas Africanos</em> que pretende olhar para o passado para nos ajudar a construir o
+                    presente e
+                    pensar novos futuros.
+                </p>
+                <p lang="pt">
+                    Concebemos um catálogo com textos que refletem sobre a mostra <em>Por Outros Cinemas Africanos</em>,
+                    escrito
+                    por Janaína Oliveira. Traremos duas entrevistas no catálogo: uma com Annabelle Aventurin, publicada
+                    na revista <em>Non Fiction</em>, na qual fala sobre processos de restauração dos filmes do cineasta
+                    Med
+                    Hondo; outra realizada por Alessandra Brito com Rejane Faria, a nossa homenageada da edição.
+                </p>
+                <p lang="pt">Convidamos a todas, todes e todos a acompanhar a programação.</p>
+                <p lang="en">We invite all of you to keep up with the program.</p>
+            </div>
         </div>
-      </div>
-    </div>
-  </section>
+    </section>
+    <section class="vinheta">
+        <div class="video">
+            <button id="button"><em class="fa fa-play" aria-hidden="true"></em></button>
+            <div id="lightbox">
+                <em id="close-btn" class="fa fa-times"></em>
+                <div id="video-wrapper">
+                    <iframe id="iframeVideo" width="960" height="540" src="https://www.youtube.com/embed/n-9G1uXElAk"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
 <?php
   getFooterOpen(); 
--- js/collapse.js
+++ js/collapse.js
@@ -1,28 +1,13 @@
 function sessionCollapsible() {
     $(".hidden").hide();
-    $(".sessionTitleDistopia").click(function () {
-        $(this).nextAll(".postBoxDistopia").slice(0, 3).toggle();
-    });
     $(".sessionTitle").click(function () {
         $(this).nextAll(".postBox").slice(0, 10).toggle();
-    });
-    $(".sessionTitleIbejis1").click(function () {
-        $(this).nextAll(".postBoxIbejis1").slice(0, 4).toggle();
-    });
-    $(".sessionTitleIbejis2").click(function () {
-        $(this).nextAll(".postBoxIbejis2").slice(0, 5).toggle();
     });
     $(".sessionTitleAfetos").click(function () {
         $(this).nextAll(".postBoxAfetos").slice(0, 12).toggle();
     });
     $(".sessionTitle1").click(function () {
-        $(this).nextAll(".postBox1").slice(0, 5).toggle();
-    });
-    $(".sessionTitle2").click(function () {
-        $(this).nextAll(".postBox2").slice(0, 12).toggle();
-    });
-    $(".sessionTitle3").click(function () {
-        $(this).nextAll(".postBox3").slice(0, 2).toggle();
+        $(this).nextAll(".postBox1").slice(0, 1).toggle();
     });
 }
 
@@ -47,14 +32,14 @@ $(document).ready(function () {
     $("#button").on("click", function () {
         $("#lightbox").fadeIn(1000);
         $(this).hide();
-        var videoURL = $("#video").prop("src");
+        var videoURL = $("#iframeVideo").prop("src");
         videoURL += "?autoplay=1";
-        $("#video").prop("src", videoURL);
+        $("#iframeVideo").prop("src", videoURL);
     });
     $("#close-btn").on("click", function () {
-        var videoURL = $("#video").prop("src");
+        var videoURL = $("#iframeVideo").prop("src");
         videoURL += "?stop=1";
-        $("#video").prop("src", videoURL);
+        $("#iframeVideo").prop("src", videoURL);
         $("#lightbox").fadeOut(500);
         $("#button").show(250);
     });
