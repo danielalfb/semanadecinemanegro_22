a81b32279e6a558d7903158830328c453a624330
Lidiane Pereira <lidianemara93@gmail.com>
2023-01-24 19:35:09 -0300

    fix: fix translate in temp_page

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 function translate() {
-  var lang = localStorage.getItem('lang');
-  if (lang) {
-    if (lang == 'pt') {
-      $('[lang="pt"]').show();
-      $('[lang="en"]').hide();
+    var lang = localStorage.getItem("lang");
+    if (lang) {
+        if (lang == "pt") {
+            $('[lang="pt"]').show();
+            $('[lang="en"]').hide();
+        } else {
+            $('[lang="en"]').show();
+            $('[lang="pt"]').hide();
+        }
     } else {
-      $('[lang="en"]').show();
-      $('[lang="pt"]').hide();
+        $('[lang="en"]').hide();
+        localStorage.setItem("lang", "pt");
     }
-  } else {
-    $('[lang="en"]').hide();
-    localStorage.setItem('lang', 'pt');
-  }
-  $('.language').click(function () {
-    $('[lang="pt"]').toggle();
-    $('[lang="en"]').toggle();
-    if (lang === 'pt') {
-      localStorage.setItem('lang', 'en');
-    } else {
-      localStorage.setItem('lang', 'pt');
-    }
-  });
+    $(".language").click(function () {
+        $('[lang="pt"]').toggle();
+        $('[lang="en"]').toggle();
+        if (lang === "pt") {
+            localStorage.setItem("lang", "en");
+        } else {
+            localStorage.setItem("lang", "pt");
+        }
+    });
 }
 // translate();
 
 (function ($) {
-  $(function () {
-    $('.menu__icon').on('click', function () {
-      $(this).closest('.navbar').toggleClass('menu_state_open');
+    $(function () {
+        $(".menu__icon").on("click", function () {
+            $(this).closest(".navbar").toggleClass("menu_state_open");
+        });
     });
-  });
 })(jQuery);
